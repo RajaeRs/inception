2521b14fa4f567f925a201ed76f8944aebebe5d9
RajaeRs <rajaerasezine@gmail.com>
2024-02-27 14:18:55 -0600

    error in DB

--- src/requirements/wordpress/tools/entrypoint_script.sh
+++ src/requirements/wordpress/tools/entrypoint_script.sh
@@ -11,7 +11,7 @@ set -e
 if [ ! -f /var/www/html/wp-config.php ]; then
     echo -e "${BLUE}First initialisation WordPress ... ${RESET}"
     wp core download --allow-root;
-    wp config create --allow-root --dbname={$MARIADB_DATABASE_NAME} --dbuser={$MARIADB_USER_NAME} --dbhost=localhost ;
+    wp config create --allow-root --dbname={$MARIADB_DATABASE_NAME} --dbuser={$MARIADB_USER_NAME} --dbhost=mariadb:3306;
     wp config shuffle-salts --allow-root;
     wp core install --allow-root  --url="{$DOMAIN_NAME}" --title="{$TITLE}" --admin_user="{$ADMIN_NAME}" --admin_password="{$ADMIN_PASSWORD}" --admin_email="{$ADMIN_EMAIL}" --skip-email ;
     wp user create --allow-root $USER_NAME $USER_EMAIL --role=administrator --porcelain;
