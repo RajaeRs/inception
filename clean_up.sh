7388bc314293fb80f37b47d3e72f93de925e4378
RajaeRs <rajaerasezine@gmail.com>
2024-03-02 08:34:48 -0600

    all work

--- clean_up.sh
+++ clean_up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker compose down;
-docker volume rm inception_mariadb-db inception_nginx_db inception_wordPress-db;
-docker rmi inception-wordpress inception-mariadb inception-nginx;
-rm -r -- data/mariadb data/nginx data/wp ;
-mkdir data/mariadb data/nginx data/wp ;
+docker volume rm mariadb-db nginx_db wordPress-db;
+docker rmi inseption_v1-wordpress inseption_v1-mariadb inseption_v1-nginx;
+rm -r -- ../../data/mariadb ../../data/nginx ../../data/wp ;
+mkdir ../../data/mariadb ../../data/nginx ../../data/wp ;
